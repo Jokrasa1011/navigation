894569de2613dc42c57e124d82acf0a8705e4349
Vova Shcherbyna <volodymyr.shcherbyna@alumni.tu-berlin.de>
2023-12-19 13:18:06 +0100

    don't wait for transform

--- core/costmap_2d/src/costmap_2d_ros.cpp
+++ core/costmap_2d/src/costmap_2d_ros.cpp
@@ -88,6 +88,8 @@ Costmap2DROS::Costmap2DROS(const std::string& name, tf2_ros::Buffer& tf) :
   ros::Time last_error = ros::Time::now();
   std::string tf_error;
   // we need to make sure that the transform between the robot base frame and the global frame is available
+  
+  if(false) //CLOCK NOT AVAILABLE
   while (ros::ok()
       && !tf_.canTransform(global_frame_, robot_base_frame_, ros::Time(), ros::Duration(0.1), &tf_error))
   {
